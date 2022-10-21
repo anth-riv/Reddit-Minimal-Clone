cf25d91a11208af7fbeae50f25491b396bf12e79
anth-riv <anthony.rivoiron@gmail.com>
2022-10-21 15:55:30 +0200

    construct post component

--- src/components/PostsHomePage/ResultsContainer.js
+++ src/components/PostsHomePage/ResultsContainer.js
@@ -1,14 +1,13 @@
 import React from "react";
 import styles from "./ResultsContainer.module.css"
+import { Post } from "../../features/post/Post";
 
 
 export const ResultsContainer = () => {
 
     return (
         <div className={styles.resultsContainer}>
-            <ul>
-                Test
-            </ul>
+            <Post />
         </div>
     )
 }
