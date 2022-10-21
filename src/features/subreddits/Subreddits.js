f942d89af8b7fca1dea45e51e12892cc578b12d9
anth-riv <anthony.rivoiron@gmail.com>
2022-10-21 14:53:51 +0200

    finish subreddit component

--- src/features/subreddits/Subreddits.js
+++ src/features/subreddits/Subreddits.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getSubreddits, selectSubreddits } from "./SubredditsSlice"
+import { getSubreddits, selectSubreddits } from "./SubredditsSlice";
+import { getSubredditPosts } from "../post/PostSlice";
 
 import styles from './Subreddits.module.css';
 
@@ -17,7 +18,8 @@ export const Subreddits = () => {
         {
           subreddits.slice(0, 14).map((subreddit) => (
             <li key={subreddit.id} className={styles.subLi}>
-              test
+              <img className={styles.subIcon} src={subreddit.icon_img || `https://api.adorable.io/avatars/25/${subreddit.display_name}`} alt={`${subreddit.display_name_prefixed}`}/>
+              <button onClick={() => dispatch(getSubredditPosts(subreddit.display_name))}>{subreddit.display_name_prefixed}</button>
             </li>
           ))
         }
